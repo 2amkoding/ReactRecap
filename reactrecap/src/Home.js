a1fa5bbaa85590cd9951c2bb641f30990f4656f8
2amkoding <2amkoding@gmail.com>
2024-08-26 18:47:49 -0500

    done

--- reactrecap/src/Home.js
+++ reactrecap/src/Home.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
 const Home = () => {
-  return (
+  
+  const handleClick = () => {
+    console.log('this is a click event');
+  }
+
+  const invokeFunction = (name) =>{
+    console.log(name + ', invoke by using an annonymous function');
+  }
+
+    return (
     <div className='home'>
-        
+        <button onClick={handleClick}>click events</button>
+        <button onClick={() => invokeFunction('coder')}>invokeFuncButton</button>
     </div>
   )
 }
